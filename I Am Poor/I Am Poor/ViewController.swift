7398f6b4e74c51df6d8584a57dc14202df02f5fe
YASH <yashpatel3645@gmail.com>
2020-08-13 13:25:42 +0530

    Initial commit

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Yash Patel on 12/08/20.
+//  Copyright © 2020 Yash Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
